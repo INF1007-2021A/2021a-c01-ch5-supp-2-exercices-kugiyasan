8d3a25a5a63a99ce5096498819c667a9d416173d
kugiyasan <kugiyasan@users.noreply.github.com>
2021-09-21 16:32:48 -0400

    Completed exercice.py

--- exercice.py
+++ exercice.py
@@ -2,28 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import math
+from typing import List
 
-def get_num_letters(text):
-	return 0
 
-def get_word_length_histogram(text):
-	return [0]
+def get_num_letters(text: str) -> int:
+    return sum(c.isalnum() for c in text)
 
-def format_histogram(histogram):
-	ROW_CHAR = "*"
 
-	return ""
+def get_word_length_histogram(text: str) -> List[int]:
+    num_letters = [get_num_letters(word) for word in text.split()]
+    histogram = [0] * (max(num_letters) + 1)
+    for n in num_letters:
+        histogram[n] += 1
+    return histogram
 
-def format_horizontal_histogram(histogram):
-	BLOCK_CHAR = "|"
-	LINE_CHAR = "¯"
 
-	return ""
+def format_histogram(histogram: List[int]) -> str:
+    ROW_CHAR = "*"
+    text = ""
+
+    for i, n in enumerate(histogram[1:]):
+        text += f"{i+1: >2} {ROW_CHAR * n}\n"
+
+    return text
+
+
+def format_horizontal_histogram(histogram: List[int]) -> str:
+    BLOCK_CHAR = "|"
+    LINE_CHAR = "¯"
+
+    histogram = histogram[1:]
+    text = ""
+
+    for i in range(max(histogram), 0, -1):
+        for n in histogram:
+            text += BLOCK_CHAR if i <= n else " "
+        text += "\n"
+
+    return text + LINE_CHAR * (len(histogram) + 1)
 
 
 if __name__ == "__main__":
-	spam = "Stop right there criminal scum! shouted the guard confidently."
-	eggs = get_word_length_histogram(spam)
-	print(eggs, "\n")
-	print(format_histogram(eggs), "\n")
-	print(format_horizontal_histogram(eggs))
+    spam = "Stop right there criminal scum! shouted the guard confidently."
+    eggs = get_word_length_histogram(spam)
+    print(eggs, "\n")
+    print(format_histogram(eggs), "\n")
+    print(format_horizontal_histogram(eggs))
